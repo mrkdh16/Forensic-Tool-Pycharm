d16116d36bfccfcb8a08f0986db8a5883f3db377
cyberls17 <mhs0001@naver.com>
2022-09-21 23:08:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import getpass
 
 
 def main():
+    file_info = []
     file_list = []
+    size = []
+    ref = []
+
     username = getpass.getuser()
     db = sqlite3.connect("./History")
 
@@ -12,14 +16,23 @@ def main():
         file_list = row[0].split('\\')
         print(file_list[-1])
 
-    for row in db.execute('SELECT target_path, total_bytes FROM downloads'):
-        file_list = row
-        if file_list[-1] / 1048576 >= 1:
-            print(round(file_list[-1]/1048576),"MB")
-        elif file_list[-1] / 1024 >= 1:
-            print(round(file_list[-1] / 1024),"KB")
+    for row in db.execute('SELECT total_bytes FROM downloads'):
+        size = row
+        if size[-1] / 1048576 >= 1:
+            print(round(size[-1]/1048576),"MB")
+        elif size[-1] / 1024 >= 1:
+            print(round(size[-1] / 1024),"KB")
         else:
-            print(round(file_list[-1]),"Bytes")
+            print(round(size[-1]),"Bytes")
+
+    for referrer in db.execute('SELECT referrer FROM downloads'):
+        ref = referrer
+        print(ref[-1])
+
+    file_info = file_list
+    file_info = size
+    file_info = ref
+    print(file_info)
 
 
 if __name__ == "__main__":
