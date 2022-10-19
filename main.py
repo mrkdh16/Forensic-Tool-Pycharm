df4300bec6a1b2d5c92f8cc70cb1ec55e77593a8
Mark <mark.dh.rhee@gmail.com>
2022-10-19 12:05:42 +0900

    배열 구분

--- main.py
+++ main.py
@@ -4,36 +4,30 @@ import getpass
 
 
 def main():
+    name_list = []
+    size_list = []
+    ref_list = []
     file_info = []
-    file_list = []
-    size = []
-    ref = []
 
     username = getpass.getuser()
-    db = sqlite3.connect("./History")
+    db = sqlite3.connect("./History")                       
 
-    for row in db.execute('SELECT target_path, total_bytes FROM downloads'):
-        file_list = row[0].split('\\')
-        print(file_list[-1])
+    for row in db.execute('SELECT target_path FROM downloads'):
+        name_list.append(row[0].split('\\')[-1])
 
     for row in db.execute('SELECT total_bytes FROM downloads'):
-        size = row
-        if size[-1] / 1048576 >= 1:
-            print(round(size[-1]/1048576),"MB")
-        elif size[-1] / 1024 >= 1:
-            print(round(size[-1] / 1024),"KB")
+        if row[-1] / 1048576 >= 1:
+            size_list += ((round(row[-1]/1048576),"MB"),)
+        elif row[-1] / 1024 >= 1:
+            size_list += ((round(row[-1] / 1024),"KB"),)
         else:
-            print(round(size[-1]),"Bytes")
+            size_list += ((round(row[-1]),"Bytes"),)
 
     for referrer in db.execute('SELECT referrer FROM downloads'):
-        ref = referrer
-        print(ref[-1])
+        file_info += ()
+        ref_list.append(referrer[-1])
 
-    file_info = file_list
-    file_info = size
-    file_info = ref
     print(file_info)
-
 
 if __name__ == "__main__":
     main()
